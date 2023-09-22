d479381ec22a7753a8915b5dbb539bee1fa81826
sugiartofahmi <sugiartofahmi@gmail.com>
2023-09-22 20:38:58 +0700

    improvement: initial setup zod

---
+++ src/entities/schemas/auth/register/index.ts
@@ -0,0 +1,11 @@
+import { z } from 'zod';
+
+export const VSRegister = z
+  .object({
+    fullname: z.string().nonempty(),
+    email: z.string().email().nonempty(),
+    password: z.string().min(8).nonempty(),
+  })
+  .required();
+
+export type TVSRegister = z.infer<typeof VSRegister>;
