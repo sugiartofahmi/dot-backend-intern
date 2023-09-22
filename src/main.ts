f4d6369f224fc54aa59d739644038be4aa329d9b
sugiartofahmi <sugiartofahmi@gmail.com>
2023-09-22 19:27:26 +0700

    improvement: initial setup prisma

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { MasterModule } from './modules';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(MasterModule);
+  const globalPrefix = 'api';
+  const url = process.env.CORS_ORIGIN;
+  const origin = url.includes(',') ? url.split(',') : url;
+  app.enableCors({
+    origin,
+    methods: ['GET', 'POST', 'DELETE', 'PUT', 'PATCH'],
+    credentials: true,
+  });
+  app.setGlobalPrefix(globalPrefix);
+
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  Logger.log(
+    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
+  );
 }
+
 bootstrap();
